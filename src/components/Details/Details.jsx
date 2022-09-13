e53d763055ae0a495715462af6e95175b128a94b
Ar1f007 <muhammadarif9416@yahoo.com>
2022-09-13 12:37:49 +0600

    detail page

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -1,10 +1,13 @@
-import { Container } from '@mui/material';
+import { Box, Container, Stack, Typography } from '@mui/material';
 import { useEffect } from 'react';
 import { connect } from 'react-redux';
 import { useLocation, useParams } from 'react-router-dom';
 import { fetchDetails } from '../../store/topRatedMoviesTvShows/action';
+import { Loader } from '../common/Loader/Loader';
 
-const Details = ({ detail, fetchDetails }) => {
+const baseURL = process.env.REACT_APP_IMAGE_BASE_PATH;
+
+const Details = ({ detail, fetchDetails, fetching }) => {
   const { id } = useParams();
   const { state } = useLocation();
 
@@ -12,13 +15,69 @@ const Details = ({ detail, fetchDetails }) => {
     fetchDetails(id, state);
   }, [id, state, fetchDetails]);
 
-  console.log(detail);
-  return <Container sx={{ py: 5 }}>{id}</Container>;
+  if (fetching) {
+    return <Loader loading={fetching} />;
+  }
+
+  if (detail) {
+    return (
+      <Container sx={{ py: 5 }}>
+        <Stack
+          direction={{
+            xs: 'column',
+            lg: 'row',
+          }}
+          spacing={5}
+        >
+          <Box width="100%" maxWidth="300" flex={1}>
+            <img src={`${baseURL}${detail?.poster_path}`} alt={detail.title} width="100%" />
+          </Box>
+          <Box flex={2}>
+            <Stack>
+              <Typography variant="h5">{detail.title}</Typography>
+              <Typography variant="body2">
+                {detail.genres
+                  ?.map((genre) => <span key={genre.name}>{genre.name}</span>)
+                  .reduce((prev, curr) => [prev, ', ', curr])}
+              </Typography>
+
+              <Typography component="i" variant="body2" my={3}>
+                {detail.tagline}
+              </Typography>
+
+              <Typography variant="h5" mb={1}>
+                Overview
+              </Typography>
+
+              <Typography variant="body2">{detail.overview}</Typography>
+
+              <Typography variant="h6" mt={3}>
+                Released
+              </Typography>
+
+              <Typography variant="body2">
+                {new Date(detail.release_date).toLocaleDateString()}
+              </Typography>
+
+              <Typography variant="h6" mt={3}>
+                Vote average
+              </Typography>
+
+              <Typography variant="body2">
+                {detail.vote_average.toFixed(1)} ({detail?.vote_count})
+              </Typography>
+            </Stack>
+          </Box>
+        </Stack>
+      </Container>
+    );
+  }
 };
 
 const mapStateToProps = (state) => {
   return {
     detail: state.topRated.detail,
+    fetching: state.topRated.loading,
   };
 };
 
